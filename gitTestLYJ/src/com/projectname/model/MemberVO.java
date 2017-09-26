19d72fa239ffda4e48c4e16e44c709c092746eb4
Administrator <Administrator@192.168.0.148>
2017-09-26 16:24:04 +0900

    MemberVO.java를 수정했음1

--- gitTestLYJ/src/com/projectname/model/MemberVO.java
+++ gitTestLYJ/src/com/projectname/model/MemberVO.java
@@ -1,5 +1,14 @@
 package com.projectname.model;
 
 public class MemberVO {
+	private String id;
 
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
 }
